0d507c295c75a997ae3e5f187e4a6e5db5f1d854
Damian Kurnik <damian14349@gmail.com>
2022-07-05 11:37:04 +0000

    update selenium test

--- test/selenium/test.py
+++ test/selenium/test.py
@@ -7,17 +7,20 @@ from selenium.webdriver.common.by import By
 application_URL = environ.get('APPLICATION_URL', 'http://localhost:5001/')
 selenium_URL = environ.get('SELENIUM_URL', 'http://192.168.44.44:4444/wd/hub')
 
-class Test()
+class Test(unittest.TestCase)
     def setUp(self):
     self.driver = webdriver.Firefox()
 
     def test(self)
         driver = self.driver
-        driver.get("APPLICATION_URL")
+        driver.get(application_URL)
         elem = driver.find_element(By.NAME, "name")
-        elem.send_keys("pycon")
+        elem.send_keys("Geralt")
         elem = driver.find_element(By.NAME, "animal")
-        elem.send_keys("pycon")
+        elem.send_keys("cat")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found.", driver.page_source)
+        driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
